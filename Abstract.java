dc85ac600647b48b8d51a7f949687ed7ce800bd9
Shubham Mishra <mey207647@mech.iitd.ac.in>
2021-10-23 18:58:48 +0530

    Add files via upload

---
+++ Abstract.java
@@ -0,0 +1,50 @@
+abstract class Oolala {
+    abstract public void greet();
+}
+class Fantasy extends Oolala {
+    public void greet() {
+        System.out.println("hahahahahahahahah");
+    }
+}
+class HaiMeri {
+    public void method(){
+        System.out.println("hahahahahahahahah");
+
+    }
+}
+
+abstract class Nomad extends Oolala {
+//    abstract void new1();
+    void fffff(){
+        System.out.println("sdgyusgdcycsjhh");
+    }
+}
+class Hulla extends Nomad{
+    public void new1(){
+        System.out.println("Gagagagagaga");
+    }
+
+    public void greet(){
+        System.out.println("Gagagagnananananaagaga");
+    }
+
+
+}
+
+
+
+
+
+
+public class Abstract {
+    public static void main(String[] args) {
+
+        Fantasy b = new Fantasy();
+        b.greet();
+
+    }
+
+
+
+
+}
