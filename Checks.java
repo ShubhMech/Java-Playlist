bf3c34b721f980a5aa197981223918969ff955a6
Shubham Mishra <mey207647@mech.iitd.ac.in>
2021-10-28 20:10:32 +0530

    Add files via upload

---
+++ Checks.java
@@ -0,0 +1,29 @@
+import bcom.New.AccessModifiers;
+import org.w3c.dom.ls.LSOutput;
+
+class Babe extends AccessModifiers{
+    static void lallu(){
+        System.out.println("HoHallaMatKaro");
+    }
+}
+
+//class Nulla{
+//    private int g= 77;
+//    int gg= 44; n
+//    int meth1(){
+//        System.out.println("Hahahahahahaha");
+//        return g;
+//    }
+//}
+//class tucha extends bcom.New.Nulla {
+//}
+
+public class Checks {
+    public static void main(String[] args) {
+        Babe bb= new Babe();
+        System.out.println(bb.ff);
+//        System.out.println(bb.ggagg);
+        System.out.println("Chilla do ek baar jor se");
+        System.out.println(bb.ganga());
+    }
+}
